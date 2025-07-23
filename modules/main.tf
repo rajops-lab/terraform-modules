9adb20da56fdb1410376ca617f30028128eda631
rajops-lab <rajeshavhd@gmail.com>
2025-07-23 20:35:52 +0530

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -1,5 +1,5 @@
 module "eks_vpc" {
-  source = "git::git@github.com:rajops-lab/terraform-modules.git//modules/01-eks_vpc?ref=v1.0.1"
+  source = "git::git@github.com:rajops-lab/terraform-modules.git//modules/01-eks_vpc?ref=v1.0.2"
 
   environment     = var.environment
   region_name     = var.region_name
@@ -9,7 +9,7 @@ module "eks_vpc" {
 }
 
 module "eks_cluster" {
-  source = "git::git@github.com:rajops-lab/terraform-modules.git//modules/02-eks_cluster?ref=v1.0.1"
+  source = "git::git@github.com:rajops-lab/terraform-modules.git//modules/02-eks_cluster?ref=v1.0.2"
 
   eks_subnet_ids = flatten([module.eks_vpc.pub_subnets, module.eks_vpc.priv_subnets])
   eks_cluster_name = var.eks_cluster_name
@@ -21,7 +21,7 @@ module "eks_cluster" {
 }
 
 module "eks_node_group" {
-  source = "git::git@github.com:rajops-lab/terraform-modules.git//modules/03-eks_node_group?ref=v1.0.1"
+  source = "git::git@github.com:rajops-lab/terraform-modules.git//modules/03-eks_node_group?ref=v1.0.2"
 
   eks_subnet_ids   = flatten([module.eks_vpc.pub_subnets, module.eks_vpc.priv_subnets])
   eks_cluster_name = module.eks_cluster.cluster_name

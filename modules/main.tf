7e17a157b7c4bef85acb62cd4c20a26acb5fadd8
rajops-lab <rajeshavhd@gmail.com>
2025-07-23 20:14:17 +0530

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -1,5 +1,5 @@
 module "eks_vpc" {
-  source = "./modules/01-eks_vpc"
+  source = "./01-eks_vpc"
 
   environment     = var.environment
   region_name     = var.region_name
@@ -9,7 +9,7 @@ module "eks_vpc" {
 }
 
 module "eks_cluster" {
-  source = "./modules/02-eks_cluster"
+  source = "./02-eks_cluster"
 
   eks_subnet_ids = flatten([module.eks_vpc.pub_subnets, module.eks_vpc.priv_subnets])
   eks_cluster_name = var.eks_cluster_name
@@ -21,7 +21,7 @@ module "eks_cluster" {
 }
 
 module "eks_node_group" {
-  source = "./modules/03-eks_node_group"
+  source = "./03-eks_node_group"
 
   eks_subnet_ids   = flatten([module.eks_vpc.pub_subnets, module.eks_vpc.priv_subnets])
   eks_cluster_name = module.eks_cluster.cluster_name

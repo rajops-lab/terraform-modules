a7217de510eeb6ccbe2a10ba0ade8ba260c2fd1b
rajops-lab <rajeshavhd@gmail.com>
2025-07-24 07:08:49 +0000

    Reset repo with new Terraform modules

--- modules/eks_node_group/variables.tf
+++ modules/eks_node_group/variables.tf
@@ -18,13 +18,10 @@ variable "node_role_name" {
   default     = "NodeRoleAccess"
 }
 
-/*
 variable "nodegroup_keypair" {
   description = "Key pair name to attach for EC2 nodes in Node group"
-  type        = string
-  default     = "eks-node-keypair"
+  type        = string  
 }
-*/
 
 variable "eks_cluster_name" {
   description = "Name of EKS Cluster, pulled from eks_cluster module"
